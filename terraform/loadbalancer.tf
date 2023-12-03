efe7f2539488853365ddb0d7db9a598fe734958f
Murillo Mamud <murillomamud@gmail.com>
2023-12-03 16:19:23 -0300

    add cert to listener

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -16,6 +16,7 @@ resource "aws_lb_target_group" "my_target_group" {
 
 
 resource "aws_lb_listener" "my_listener" {
+  certificate_arn   = var.cert_arn
   load_balancer_arn = aws_lb.my_lb.arn
   port              = "443"
   protocol          = "HTTPS"
